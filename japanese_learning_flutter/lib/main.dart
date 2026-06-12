121c0a2099b7d4195208b14fca18ce22549f88be
ET-DIII <hhduong2305@gmail.com>
2026-06-12 23:00:21 +0700

    demo gia dien flasshcard (duong)

--- japanese_learning_flutter/lib/main.dart
+++ japanese_learning_flutter/lib/main.dart
@@ -6,6 +6,8 @@ import 'views/account/profile_screen.dart';
 import 'views/account/settings_screen.dart';
 import 'views/account/news_screen.dart';
 import 'views/account/sentence_screen.dart';
+import 'views/flashcard/create_flashcard_screen.dart';
+import 'views/flashcard/my_sets_screen.dart';
 
 void main() async {
   // Kích hoạt Flutter sẵn sàng trước khi chạy lệnh ẩn (async)
@@ -31,7 +33,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         useMaterial3: true,
       ),
-      home: const LoginScreen(), // Đặt màn hình Đăng nhập xuất hiện đầu tiên khi mở app
+      home: const MySetsScreen(), // Đặt màn hình Đăng nhập xuất hiện đầu tiên khi mở app
     );
   }
 }
