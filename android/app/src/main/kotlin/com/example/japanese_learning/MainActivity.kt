c7af1b09b00fc243ffd13baeaf7c586d22e4835c
ET-DIII <hhduong2305@gmail.com>
2026-06-14 22:54:56 +0700

    fix loi merge code

--- android/app/src/main/kotlin/com/example/japanese_learning/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.japanese_learning
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- lib/services/account/auth_exception.dart
+++
@@ -1,9 +0,0 @@
-class AuthException implements Exception {
-  final String message;
-  final String? code;
-
-  const AuthException(this.message, {this.code});
-
-  @override
-  String toString() => message;
-}
